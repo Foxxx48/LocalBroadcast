a1bb5aa25ea998bb02d316018a12e915f1e4b04e
Foxxx48 <Foxxdim@mail.ru>
2022-10-04 17:25:08 +0300

    Add Service, Broadcast Receiver

--- app/src/main/java/com/fox/localbroadcast/MainActivity.kt
+++ app/src/main/java/com/fox/localbroadcast/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.fox.localbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fox.localbroadcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding?: throw RuntimeException("ActivityMainBinding = null")
+
+    private val reciever = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == MyService.LOADED) {
+                val percent = intent.getIntExtra(MyService.EXTRA_KEY, 100500)
+                binding.progressBar.progress = percent
+            }
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startService(
+                Intent(this,
+                    MyService::class.java
+                )
+            )
+        }
+
+        val intentFilter = IntentFilter().apply {
+            addAction(MyService.LOADED)
+        }
+        registerReceiver(reciever, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+        unregisterReceiver(reciever)
     }
 }
---
+++ app/src/main/java/com/fox/localbroadcast/MyService.kt
@@ -0,0 +1,30 @@
+package com.fox.localbroadcast
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import kotlin.concurrent.thread
+
+class MyService: Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        thread {
+            for (i in 1 .. 10) {
+                Thread.sleep(1000)
+                Intent(LOADED).apply {
+                    putExtra(EXTRA_KEY, i * 10)
+                    sendBroadcast(this)
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    companion object{
+        const val LOADED = "loaded"
+        const val EXTRA_KEY = "extra_key"
+    }
+}
